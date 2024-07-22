72b328c76b2773d80fff1fd7e767f73f3039d53e
saulolv <saulosantos229@gmail.com>
2024-07-22 13:13:58 -0300

    Initial commit

---
+++ src/main/java/gov/api_confea/ApiConfeaApplication.java
@@ -0,0 +1,13 @@
+package gov.api_confea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConfeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiConfeaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gov/api_confea/ApiConfeaApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.api_confea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConfeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
